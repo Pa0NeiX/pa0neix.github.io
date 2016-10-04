29792bb527bfd5ab35ea719ec70fa6b27fc93b44
pnx <Pa0NeiX@gmail.com>
2016-10-04 13:43:05 +0300

    Update testScript.js

--- testScript.js
+++ testScript.js
@@ -1,5 +1,5 @@
 var myPrettyCode = function() {
-    var containers = document.getElementsByClassName('username_container');
+    /*var containers = document.getElementsByClassName('username_container');
     var username = document.getElementsByClassName('log_in6')[0].innerText;
     for(var i = 0; i < containers.length; i++)
     {
@@ -7,5 +7,6 @@ var myPrettyCode = function() {
         {
             document.getElementsByClassName('usertitle')[i].innerHTML = "<img src='http://i.imgur.com/j0TLu1W.png' alt='MobileSubstrate Hacker' />";
         }
-    }
+    }*/
+    alert("k");
 };
