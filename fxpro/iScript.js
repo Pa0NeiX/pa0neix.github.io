6c715f74a10b6f1a0e941e4a14348db10752c165
pnx <Pa0NeiX@gmail.com>
2016-10-13 21:30:40 +0300

    Update iScript.js

--- fxpro/iScript.js
+++ fxpro/iScript.js
@@ -1,4 +1,6 @@
-if (typeof injected == 'undefined') {
-  alert('sup');
-  var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
+if(document.URL != "https://www.fxp.co.il/") {
+  if (typeof injected == 'undefined') {
+    alert('sup');
+    var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
+  }
 }
