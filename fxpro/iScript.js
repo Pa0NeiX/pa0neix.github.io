8c1f5c930196aa9585ef9c86f586f144fd1fe778
pnx <Pa0NeiX@gmail.com>
2016-10-13 22:04:23 +0300

    Update iScript.js

--- fxpro/iScript.js
+++ fxpro/iScript.js
@@ -1,9 +1,12 @@
 if(document.URL != "https://www.fxp.co.il/") {
-  if (typeof injected == 'undefined') {
-    var taboola = document.getElementsByClassName('taboola-mobile-below-article-thumbnails');
-    for(var i = 0; i < taboola.length; i++) {
-     taboola[i].remove(); 
+    if (typeof injected == 'undefined') 
+    {
+        setInterval(function () {
+            var taboola = document.getElementsByClassName('taboola-mobile-below-article-thumbnails');
+            for (var i = 0; i < taboola.length; i++) {
+                taboola[i].remove();
+            }
+        }, 5000);
+        var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
     }
-    var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
-  }
 }
