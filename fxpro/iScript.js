4043cb962981ff991954d28a6174ecbc4196fb2f
pnx <Pa0NeiX@gmail.com>
2016-10-13 22:26:13 +0300

    Update iScript.js

--- fxpro/iScript.js
+++ fxpro/iScript.js
@@ -1,12 +1,18 @@
+function detectAndRemove() {
+    setTimeout(function () {
+        if (document.getElementById('taboola-mobile-below-article-thumbnails') === null) {
+            detectAndRemove();
+        }
+        else {
+            document.getElementById('taboola-mobile-below-article-thumbnails').remove();
+        }
+    }, 420);
+}
+
 if(document.URL != "https://www.fxp.co.il/") {
     if (typeof injected == 'undefined') 
     {
-        setInterval(function () {
-            var taboola = document.getElementsByClassName('taboola-mobile-below-article-thumbnails');
-            for (var i = 0; i < taboola.length; i++) {
-                taboola[i].remove();
-            }
-        }, 5000);
+        detectAndRemove();
         var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
     }
 }
