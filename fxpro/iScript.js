618306800e232ac27850bedadf354bbf2325d7bd
pnx <Pa0NeiX@gmail.com>
2016-10-14 17:59:37 +0300

    Update iScript.js

--- fxpro/iScript.js
+++ fxpro/iScript.js
@@ -1,26 +1,3 @@
-/*function detectAndRemove() {
-    setTimeout(function() {
-        if (document.getElementById('taboola-mobile-below-article-thumbnails') === null) {
-            detectAndRemove();
-        }
-        else {
-            document.getElementById('taboola-mobile-below-article-thumbnails').remove();
-        }
-    }, 420);
-}
-
-function removeExtra()
-{
-    if(document.URL != "https://www.fxp.co.il/") {
-        if (typeof injected == 'undefined') 
-        {
-            detectAndRemove();
-            var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
-        }
-    }
-}
-*/
-
 function detectAndRemove(rem) {
     setTimeout(function () {
         if (document.getElementById(rem) === null) {
@@ -32,23 +9,32 @@ function detectAndRemove(rem) {
     }, 420);
 }
 
+function alreadySent(id) {
+    writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var ' + id + ' = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
+}
+
 function removeExtra() {
     if (document.URL == "https://www.fxp.co.il/") 
         return;
 
-    //if (typeof injected == 'undefined') {
-    detectAndRemove('taboola-mobile-below-article-thumbnails');
+    if (typeof funcExtra == 'undefined') {
+        ('taboola-mobile-below-article-thumbnails');
 
-    for(var i = 0; i < document.getElementsByTagName('iframe').length; i++) { 
-        document.getElementsByTagName('iframe')[i].remove(); 
+        for(var i = 0; i < document.getElementsByTagName('iframe').length; i++) { 
+            document.getElementsByTagName('iframe')[i].remove(); 
+        }
+
+        alert('x');
+        alreadySent('funcExtra');
     }
-
-    alert('x');
-
-        //var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
-    //}
 }
 
 function adBlock() {
+    if (document.URL == "https://www.fxp.co.il/") 
+        return;
 
+    if (typeof funcBlock == 'undefined') {
+        detectAndRemove('google_image_div');
+        alreadySent('funcBlock');
+    }
 }
