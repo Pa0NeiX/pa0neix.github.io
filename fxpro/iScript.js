b9a6928b3a967a5f340e6cfd63ecae0c94152cae
pnx <Pa0NeiX@gmail.com>
2016-10-14 18:43:05 +0300

    Update iScript.js

--- fxpro/iScript.js
+++ fxpro/iScript.js
@@ -9,8 +9,42 @@ function detectAndRemove(rem) {
     }, 420);
 }
 
+function iDetectAndRemove(rem) {
+    setTimeout(function () {
+        if (rem === null) {
+            iDetectAndRemove(rem);
+        }
+        else {
+            rem.remove();
+        }
+    }, 420);
+}
+
 function alreadySent(id) {
     writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var ' + id + ' = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
+}
+
+function adBlock() {
+    if (typeof funcBlock == 'undefined') {
+        iDetectAndRemove(document.getElementById('google_image_div'));
+
+        var iLink = document.getElementsByTagName('a');
+        for (var i = 0; i < iLink.length; i++) {
+            if (iLink[i].target == '_parent' && iLink[i].href == "http://bit.ly/2dZ647j") {
+                iDetectAndRemove(iLink[i]);
+            }
+            if (iLink[i].target == '_blank') {
+                iDetectAndRemove(iLink[i]);
+            }
+        }
+
+        var iFrames = document.getElementsByTagName('iframe');
+        for (var i = 0; i < iFrames.length; i++) {
+            //iFrames[i].remove();
+            iDetectAndRemove(iFrames[i]);
+        }
+        alreadySent('funcBlock');
+    }
 }
 
 function removeExtra() {
@@ -18,21 +52,7 @@ function removeExtra() {
         return;
 
     if (typeof funcExtra == 'undefined') {
-        detectAndRemove('taboola-mobile-below-article-thumbnails');
-
-        for(var i = 0; i < document.getElementsByTagName('iframe').length; i++) { 
-            document.getElementsByTagName('iframe')[i].remove(); 
-        }
+        iDetectAndRemove(document.getElementById('taboola-mobile-below-article-thumbnails'));
         alreadySent('funcExtra');
-    }
-}
-
-function adBlock() {
-    if (document.URL == "https://www.fxp.co.il/") 
-        return;
-
-    if (typeof funcBlock == 'undefined') {
-        detectAndRemove('google_image_div');
-        alreadySent('funcBlock');
     }
 }
