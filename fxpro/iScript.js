8f2dc9922fd8273e1e69dda8ebaef36001d65b07
pnx <Pa0NeiX@gmail.com>
2016-10-14 17:51:47 +0300

    Update iScript.js

--- fxpro/iScript.js
+++ fxpro/iScript.js
@@ -1,4 +1,4 @@
-function detectAndRemove() {
+/*function detectAndRemove() {
     setTimeout(function() {
         if (document.getElementById('taboola-mobile-below-article-thumbnails') === null) {
             detectAndRemove();
@@ -18,4 +18,37 @@ function removeExtra()
             var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
         }
     }
+}
+*/
+
+function detectAndRemove(rem) {
+    setTimeout(function () {
+        if (document.getElementById(rem) === null) {
+            detectAndRemove(rem);
+        }
+        else {
+            document.getElementById(rem).remove();
+        }
+    }, 420);
+}
+
+function removeExtra() {
+    if (document.URL == "https://www.fxp.co.il/") 
+        return;
+
+    //if (typeof injected == 'undefined') {
+    detectAndRemove('taboola-mobile-below-article-thumbnails');
+
+    for(var i = 0; i < document.getElementsByTagName('iframe').length; i++) { 
+        document.getElementsByTagName('iframe')[i].remove(); 
+    }
+
+    alert('x');
+
+        //var writeRidden = document.createElement('script'); writeRidden.type = 'text/javascript'; writeRidden.innerHTML = 'var injected = true'; document.getElementsByTagName('head')[0].appendChild(writeRidden);
+    //}
+}
+
+function adBlock() {
+
 }
