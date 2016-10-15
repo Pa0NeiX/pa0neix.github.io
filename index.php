785f91579317c11c886d236aef19460d831f1806
pnx <Pa0NeiX@gmail.com>
2016-10-15 14:05:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,54 @@
+---
+permalink: /404.html
+---
+
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset=utf-8>
     <title>Redirecting...</title>
-    <link rel=canonical href="http://www.lunesu.com/index.html">
-    <meta http-equiv=refresh content="1; url=/notfound.html">
+    <style>
+        body {
+            background-color: black;
+            color: white;
+        }
+        .blocker {
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            width: 100%;
+            height: 100%;
+        }
+
+        .note {
+            position: fixed;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            text-align: center;
+            line-height: 10px;
+            text-shadow: 1px 1px 0px #000, -1px -1px 0px #000, 1px -1px 0px #000, -1px 1px 0px #000, 1px 1px 0px #000;
+        }
+        .note > h1 {
+            font-size: 200px;
+        }
+        .note > h2 {
+            line-height: normal;
+            font-size: 70px;
+        }
+
+    </style>
   </head>
   <body>
-    <h1>Redirecting...</h1>
-    <a href="http://www.lunesu.com/">Click here if you are not redirected.</a>
+    <div style="position: fixed; z-index: -99; width: 100%; height: 100%">
+        <iframe frameborder="0" style="height: 100%; width: 100%" src="https://www.youtube.com/embed/YuxtSCp1LiA?autoplay=1&controls=0&showinfo=0&loop=1"></iframe>
+    </div>
+    <!-- <div class="blocker"></div>-->
+    <div class="note">
+        <h1>404</h1>
+        <h2>עכשיו תראה סבתא שעושה צלחות</h2>
+    </div>
     <script>var n=location.href.replace("index.php?/", "");if (n!=location.href)location=n;</script>
   </body>
 </html>
