c9054d12bfbcaa5c3bd3cb156fc432fa5ebaaf78
pnx <pa0neix@gmail.com>
2017-12-23 21:17:56 +0200

    k

--- funcs.js
+++ funcs.js
@@ -1,3 +1,22 @@
+function xhr(ARG) {
+  var xhttp = new XMLHttpRequest();
+  if(ARG['type'])
+    xhttp.responseType = ARG['type'];
+  xhttp.addEventListener('readystatechange', function () {
+  if(xhttp.status == 200 && xhttp.readyState == 4 && ARG['func'])
+    ARG['func'](xhttp);
+  });
+  xhttp.open(ARG['method'], ARG['url'], ARG['async'] ? ARG['async'] : ARG['method'] == 'GET' ? true : false);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  if(ARG['payload']) {
+    var data = '';
+    Object.keys(ARG['payload']).forEach(function (e, i) {
+      data += e + '=' + Object.values(ARG['payload'])[i] + (Object.keys(ARG['payload']).length - 1 == i ? '' : '&');
+    });
+  }
+  xhttp.send(ARG['payload'] ? data: null);
+}
+
 function loadFile(path, then) {
   var xhttp = new XMLHttpRequest();
   xhttp.open('get', path, true);
@@ -16,3 +35,22 @@ function insertFile(file, intoElem) {
   });
   return x;
 }
+
+(function() {
+  var x = document.createComment(`
+  .....
+   /.////.
+     ./////.       .................  ................  ...          ...
+  .///../////.     ///////////////// .////////////////.  .//.      .//.
+     .//...////.                 /// .//           .//.    .///..///.
+      ./// ./////. ///////////////// .//           .//.      .////.
+    .//...////..   ///               .//           .//.    .///..///.
+  .///../////.     ///               .//           .//.  ///.      .///      .//
+    ../////.       ...                ..            ..  ...          ...      ..
+  .//////.
+  ......
+
+  / . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . /
+  `);
+  document.insertBefore(x, document.querySelector('html'));
+})();
